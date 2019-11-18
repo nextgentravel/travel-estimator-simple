398de04cdc802d0ae784a4688f5e39a6fc4325ae
Mike Jenkins <kinetic@icloud.com>
2019-11-18 10:34:30 -0500

    Added meals modal

---
+++ src/components/MealsModal.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="modal">
+        <div class="modal-dialog">
+            <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title">Modal title</h5>
+                <button type="button" class="close" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <p>Modal body text goes here.</p>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-primary">Save changes</button>
+                <button type="button" class="btn btn-secondary">Close</button>
+            </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import moment from 'moment'
+export default {
+  name: 'MealsModal',
+  mounted() {
+  },
+  data: function() {
+    return {
+      moment
+    }
+  },
+  methods: {
+  },
+  computed: {
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
